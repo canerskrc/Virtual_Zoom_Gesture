121ca19303dbe387e30a814ec630f0e08e9378c4
canersekerci <62672535+canerskrc@users.noreply.github.com>
2023-12-17 01:41:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)  # Kameranın indexini değiştirmeyi deneyin, 0 veya 1 olabilir.
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = HandDetector(detectionCon=0.7)
+startDist = None
+scale = 0
+cx, cy = 500, 500
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+
+    if hands:  # Eğer el algılandıysa
+        if len(hands) == 2:
+            if detector.fingersUp(hands[0]) == [1, 1, 0, 0, 0] and detector.fingersUp(hands[1]) == [1, 1, 0, 0, 0]:
+                lmList1 = hands[0]["lmList"]
+                lmList2 = hands[1]["lmList"]
+
+                if startDist is None:
+                    length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
+                    startDist = length
+
+                length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
+                scale = int((length - startDist) // 2)
+                cx, cy = info[4:]
+                print(scale)
+        else:
+            startDist = None
+
+        try:
+            img1 = cv2.imread("cvarduino.jpg")
+            h1, w1, _ = img1.shape
+            newH, newW = ((h1 + scale) // 2) * 2, ((w1 + scale) // 2) * 2
+            img1 = cv2.resize(img1, (newW, newH))
+
+            img[cy - newH // 2:cy + newH // 2, cx - newW // 2:cx + newW // 2] = img1
+        except Exception as e:
+            print(f"Error: {e}")
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
